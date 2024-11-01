f1a7de0cc153f93dbcbb08e59d29f33c45fbf383
LAPTOP-6BUUF8U8\MY LE <dammaynholuyenthuyen@gmail.com>
2024-11-01 23:39:49 +0700

    Thêm file l_seg_display.v vào thư mục mô phỏng

---
+++ project_2.srcs/sim_1/new/l_seg_display.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/31/2024 12:22:52 AM
+// Design Name: 
+// Module Name: l_seg_display
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module l_seg_display(
+
+    );
+endmodule
