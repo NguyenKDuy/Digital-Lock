cc3abe2de24d86ea35f4c42c50cc882671b48729
hieuduongk22bk <hieu.duongk22bk@hcmut.edu.vn>
2024-12-02 20:10:33 +0700

    add three time button counter to access new password mode

---
+++ sources_2_12/h_newlock_v2.v
@@ -0,0 +1,70 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/01/2024 10:24:02 AM
+// Design Name: 
+// Module Name: h_newlock_v2
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module h_newlock_v2(
+        input 	enb_inp,
+                enb_set,
+          		exit_button,
+          		enough,
+          		[15:0] value_16bit,
+          		confirm_button,
+         		clk_in,
+        output 	reg disable_cnt = 'd0,
+                reg [15:0] password = 16'd0
+    );
+
+           reg[3:0] three_time_counter=4'd0;
+           wire button_push_confirm, button_push_exit, button_push_3s_exit;
+           button_push B0(clk_in, confirm_button, button_push_confirm);
+           button_press B2(clk_in, exit_button,button_push_3s_exit);
+           reg dis_1 =1'd0;
+           reg dis_2 =1'd0;
+           reg dis_3 =1'd0;
+    always@(posedge button_push_confirm)
+    begin
+        if (enb_set == 1'd1 && enb_inp == 1'd1) begin
+            if(button_push_confirm) begin
+                three_time_counter <= three_time_counter + 4'd1;
+            if(three_time_counter >= 4'd2) begin
+                dis_1 <= ~dis_1;
+                three_time_counter <= 4'd0;
+                end   
+            end
+       end
+    end
+      always@(posedge button_push_3s_exit)
+      begin
+          if(button_push_3s_exit && enough) begin
+            //password <= value_16bit;
+            password <= value_16bit;
+            dis_2 <= ~dis_2;
+            end
+          else dis_2<= ~dis_2;
+                end
+                
+       always@(posedge clk_in) begin
+       if(dis_1 == 1'd1 && dis_2 == 1'd0) disable_cnt=1'd1;
+       else if(dis_1 == 1'd0 && dis_2 == 1'd1) disable_cnt=1'd1;
+       else  disable_cnt=1'd0;
+      
+      end
+endmodule
